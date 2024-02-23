8fd6563cc2ab61ef5a3ab36e5b8ff8653522af87
Ryan Huynh <rhuynh7@hotmail.com>
2024-02-23 13:57:34 -0700

    adding all files to repository

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,49 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const transpose = function (matrix) {
 
-module.exports = wordSearch
+    const numRows = matrix.length;
+    const numCols = matrix[0].length;
+  
+    // Create a new empty transposed matrix
+    const transposedMatrix = [];
+  
+    for (let i = 0; i < numCols; i++) {
+      transposedMatrix[i] = []; // Initialize each row
+      for (let j = 0; j < numRows; j++) {
+        transposedMatrix[i][j] = matrix[j][i]; // Swap rows with columns
+      };
+    };
+  
+    return transposedMatrix;
+  };
+  
+  const wordSearch = (letters, word) => { 
+      // if  searched for word is empty
+      if (word.length < 1 || typeof word !== 'string') {
+          return false;
+      }
+  
+      const horizontalJoin = letters.map(ls => ls.join(''))
+      for (const l of horizontalJoin) {
+          if (l.includes(word)) {
+            return true;
+          }
+      }
+      
+      let newArray = transpose(letters);
+      const verticalJoin = newArray.map(ls => ls.join(''))
+      for (const l of verticalJoin) {
+          if (l.includes(word)) {
+            return true;
+          };
+        }
+        return false;   
+  }
+  
+  
+  /* 
+  The map() method of Array instances creates a new array 
+  populated with the results of calling a provided 
+  function on every element in the calling array.
+  */
+  
+  module.exports = wordSearch
